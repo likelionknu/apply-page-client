9400f96a9ab49cd97fde60831aa22615f70b2eba
jongmin <bergi4025@gmail.com>
2026-01-16 18:44:49 +0900

    #5 style: a 태그 전역 스타일 및 반응형 고려 구조 수정

--- src/shared/components/Header.tsx
+++ src/shared/components/Header.tsx
@@ -6,23 +6,28 @@ function Header() {
   return (
     <header>
       <nav>
-        <div className="left">
+        <div className="header-left">
           <div className="logo">
             <img src={logoImg} alt="knu" className="logo-img" />
             <p className="logo-text">LIKELION KNU</p>
           </div>
-          <div className="nav-menu">
-            <p>Projects</p>
-            <p className="bar"></p>
-            <p>Apply</p>
-            <p className="bar"></p>
-            <p>Part</p>
+          <div className="desktop-menu">
+            <a>Projects</a>
+            <span className="menu-divider"></span>
+            <a>Apply</a>
+            <span className="menu-divider"></span>
+            <a>Part</a>
           </div>
         </div>
-        <div className="right">
-          <div className="google">
+        <div className="header-right">
+          <div className="google-action">
             <img src={googleImg} alt="google" className="google-img" />
             <p className="google-text">Sign in with Google</p>
+          </div>
+          <div className="menu-toggle">
+            <div className="bar"></div>
+            <div className="bar"></div>
+            <div className="bar"></div>
           </div>
         </div>
       </nav>
