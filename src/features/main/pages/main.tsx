32cddcd2becd73a72910f19547eb2072f637ec85
chanju <chanju030303@naver.com>
2026-01-19 18:14:16 +0900

    #15 feat: 메인페이지 레이아웃 생성 임시 커밋

---
+++ src/features/main/pages/main.tsx
@@ -0,0 +1,251 @@
+import "../pages/main.css";
+import img from "@/assets/images/FigmaImg.png";
+
+const MainPage = () => {
+  interface MainPageTextComponentProps {
+    titleText: string;
+    subText: React.ReactNode;
+  }
+
+  const MainPageTextComponent = ({
+    titleText,
+    subText,
+  }: MainPageTextComponentProps) => {
+    return (
+      <div className="bg-blue flex flex-col items-center justify-center gap-7.5">
+        <div className="font-pretendard text-center text-[48px] leading-[140%] font-semibold tracking-[-0.025em]">
+          {titleText}
+        </div>
+
+        <div className="font-inter text-center text-[24px] leading-[140%] font-medium tracking-[-0.025em]">
+          {subText}
+        </div>
+      </div>
+    );
+  };
+
+  interface PartIntroduceComponentProps {
+    titleText: string;
+    titleKoreanText: string;
+    subText: string;
+    img: string;
+  }
+
+  const PartIntroduceComponent = ({
+    titleText,
+    titleKoreanText,
+    subText,
+    img,
+  }: PartIntroduceComponentProps) => {
+    return (
+      <div className="flex h-111.5 w-83.25 flex-col items-center justify-between">
+        <div className="flex h-72 w-80 items-center justify-center rounded-[44px] border border-white bg-[radial-gradient(ellipse_50.00%_50.00%_at_50.00%_50.00%,rgba(0,112.05,255,0.49)_3%,rgba(0,67.23,153,0.06)_100%)] shadow-[0px_5px_20px_4px_rgba(158,216,245,0.53)]">
+          <img src={img} />
+        </div>
+        <div className="PartIntroduceComponentTextBox">
+          <div className="PartIntroduceComponentTextBoxTwo">
+            <div className="PartIntroduceComponentTitle">{titleText}</div>
+            <div className="PartIntroduceComponentTitleKorean">
+              {titleKoreanText}
+            </div>
+          </div>
+          <div className="PartIntroduceComponentSub">{subText}</div>
+        </div>
+      </div>
+    );
+  };
+
+  interface MovingContainerProps {
+    children: React.ReactNode;
+  }
+
+  const MovingContainer = ({ children }: MovingContainerProps) => {
+    return <div className="MovingSectionContainer">{children}</div>;
+  };
+
+  interface MovingItemProps {
+    delay: number;
+    children: React.ReactNode;
+  }
+
+  const MovingRightItem = ({ children, delay }: MovingItemProps) => {
+    return (
+      <div
+        className="MovingRightBox"
+        style={{
+          animationDelay: `-${delay}s`,
+        }}
+      >
+        {children}
+      </div>
+    );
+  };
+
+  const MovingLeftItem = ({ children, delay }: MovingItemProps) => {
+    return (
+      <div
+        className="MovingLeftItem"
+        style={{
+          animationDelay: `-${delay}s`,
+        }}
+      >
+        {children}
+      </div>
+    );
+  };
+
+  interface MovingItemContainer {
+    titleText: string;
+    subText: string;
+    img: string;
+  }
+
+  const MovingItemContainer = ({
+    titleText,
+    subText,
+    img,
+  }: MovingItemContainer) => {
+    return (
+      <div className="MovingItemContainer">
+        <div className="MovingItemTitle">{titleText}</div>
+        <img src={`src/assets/images/${img}.png`} />
+        <div className="MovingItemSub">{subText}</div>
+      </div>
+    );
+  };
+
+  return (
+    <div>
+      <div className="MainPageFrame">
+        <div className="MainPageFristSection"></div>
+        <div className="MainPageSecondSection">
+          <MainPageTextComponent
+            titleText="멋쟁이사자처럼 강남대학교"
+            subText={
+              <>
+                멋쟁이사자처럼 대학은 테크 기반의 아이디어 실현을 위한 전국 최대
+                규모의 대학 연합 창업 동아리예요. 현재 전국 60개의 대학과
+                <br />
+                500여명의 운영진이 멋쟁이사자처럼과 함께 혁신적인 서비스를
+                개발하며 개인과 사회에 의미 있는 변화가 되기를 추구해요.
+              </>
+            }
+          ></MainPageTextComponent>
+          <div className="DistrictImgFrame">
+            <img src="src/assets/images/District.png" />
+          </div>
+        </div>
+        <div className="PartIntroduceSection">
+          <MainPageTextComponent
+            titleText="파트 소개"
+            subText={
+              <>
+                강남대학교 멋쟁이사자처럼에는 총 4개의 파트가 존재하며, 아이디어
+                실현을 위해
+                <br />
+                모든 파트가 지속적으로 소통하며 발전을 향한 발걸음을 멈추지
+                않아요.
+              </>
+            }
+          ></MainPageTextComponent>
+          <div className="PartIntroduceSectionContainer">
+            <PartIntroduceComponent
+              titleText="PM"
+              titleKoreanText="기획"
+              subText="상상을 현실과 이상적인 맞물림"
+              img={img}
+            />
+            <PartIntroduceComponent
+              titleText="DE"
+              titleKoreanText="디자인"
+              subText="끝없는 정교함과 아름다움"
+              img="Magnifier"
+            />
+            <PartIntroduceComponent
+              titleText="BE"
+              titleKoreanText="백엔드"
+              subText="보이지 않는 구체화, 구조의 안정성"
+              img="Folder"
+            />
+            <PartIntroduceComponent
+              titleText="FE"
+              titleKoreanText="프론트"
+              subText="픽셀로 구현하는 그림"
+              img="Laptop"
+            />
+          </div>
+        </div>
+        <div className="MainPageSpaceTwo"></div>
+        <div className="ProjectIntroduceSection">
+          <MainPageTextComponent
+            titleText="프로젝트 소개"
+            subText="기술의 혁신과 아이디어의 결합을 위해 매 기수마다 다양한 프로젝트를 진행해요."
+          ></MainPageTextComponent>
+          <div className="MainPageSpaceOne" />
+          <MovingContainer>
+            <MovingRightItem delay={0}>
+              <MovingItemContainer
+                titleText="Knock"
+                subText="등기부등본 분석 기반 전세사기 예방 AI 서비스 "
+                img="Knock"
+              />
+            </MovingRightItem>
+            <MovingRightItem delay={4}>
+              <MovingItemContainer
+                titleText="가봄"
+                subText="AI 추천 및 방문인증 스탬프 수집 지역 경제 활성화 플랫폼"
+                img="GaBoom"
+              />
+            </MovingRightItem>
+            <MovingRightItem delay={8}>
+              <MovingItemContainer
+                titleText="market Bee"
+                subText=" 소상공인을 위한 마케팅 및 관리 플랫폼"
+                img="MarketBee"
+              />
+            </MovingRightItem>
+            <MovingRightItem delay={12}>
+              <MovingItemContainer
+                titleText="Rerise"
+                subText="은둔청년의 일상 회복을 돕는 서비스"
+                img="Rerise"
+              />
+            </MovingRightItem>
+          </MovingContainer>
+          <MovingContainer>
+            <MovingLeftItem delay={0}>
+              <MovingItemContainer
+                titleText="Knock"
+                subText="등기부등본 분석 기반 전세사기 예방 AI 서비스 "
+                img="Knock"
+              />
+            </MovingLeftItem>
+            <MovingLeftItem delay={4}>
+              <MovingItemContainer
+                titleText="Lococo"
+                subText="AI 지역 출제 관광 / 큐레이션 서비스"
+                img="Lococo"
+              />
+            </MovingLeftItem>
+            <MovingLeftItem delay={8}>
+              <MovingItemContainer
+                titleText="잔치"
+                subText=" 개인 맞춤형 축제 및 여행 코스 추천 플랫폼"
+                img="Janchi"
+              />
+            </MovingLeftItem>
+            <MovingLeftItem delay={12}>
+              <MovingItemContainer
+                titleText="Rerise"
+                subText="은둔청년의 일상 회복을 돕는 서비스"
+                img="Rerise"
+              />
+            </MovingLeftItem>
+          </MovingContainer>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MainPage;
--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import PendingPage from "../shared/pages/PendingPage";
+import MainPage from "../features/main/pages/main";
 
 function AppRouter() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="*" element={<PendingPage />} />
+        <Route path="/main" element={<MainPage />} />
       </Routes>
     </BrowserRouter>
   );
