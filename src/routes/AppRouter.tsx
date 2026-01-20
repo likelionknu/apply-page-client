48b7948eee9ac759cf13f2105726bbc31d451e73
pyw424 <parkyw0424@naver.com>
2026-01-20 21:25:33 +0900

    feat: 파트별 소개 페이지 ui 구성

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import PendingPage from "../shared/pages/PendingPage";
+import PartMainPage from "../features/partpage/pages/PartMainPage";
 
 function AppRouter() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/part/:part" element={<PartMainPage />} />
         <Route path="*" element={<PendingPage />} />
       </Routes>
     </BrowserRouter>
