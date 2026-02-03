855484506d9df9ed9eae101e2f6d26d78cb41e9d
pyw424 <parkyw0424@naver.com>
2026-02-03 13:58:01 +0900

    #16 rename: part 폴더명 변경

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import PendingPage from "../shared/pages/PendingPage";
-import PartMainPage from "../features/partpage/pages/PartMainPage";
+import PartMainPage from "../features/part/pages/PartMainPage";
 import MainPage from "../features/main/mainpage/MainPage";
 import RecruitPage from "../features/recruit/pages/RecruitPage";
 import ProjectDetailPage from "../features/project/pages/ProjectPage";
