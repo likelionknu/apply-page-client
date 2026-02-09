908d19c9df6c3f87b13e6b977c0eff8fba6c3dba
jongmin <bergi4025@gmail.com>
2026-02-10 02:12:06 +0900

    #55 style: 헤더 모바일 추가 중

--- src/shared/components/Header.tsx
+++ src/shared/components/Header.tsx
@@ -4,6 +4,7 @@ import GoogleLogin from "@shared/apis/GoogleLogin";
 import logoImg from "../assets/logo.png";
 import googleImg from "../assets/google.png";
 import userImg from "../assets/user.png";
+import { useState } from "react";
 
 const ACTIVE_PART = ["/part/PM", "/part/DE", "/part/BE", "/part/FE"];
 
@@ -14,13 +15,14 @@ const getActiveClass = (isActive: boolean) => {
 };
 
 const ToggleBar = () => (
-  <div className="bg-gray1 h-0.5 w-4.5 rounded-[20px]"></div>
+  <div className="h-0.5 w-4.5 rounded-[20px] bg-white"></div>
 );
 
 function Header() {
   const location = useLocation();
   const navigate = useNavigate();
   const isPartPage = ACTIVE_PART.includes(location.pathname);
+  const [isShow, setIsShow] = useState<boolean>(false);
   const isLogin = sessionStorage.getItem("accessToken");
   const name = "홍길동";
 
@@ -31,16 +33,16 @@ function Header() {
   // }, [pathname]);
 
   return (
-    <header className="text-white1 bg-black1 sticky top-0 z-100 flex h-20 w-full">
-      <nav className="mx-auto flex w-full max-w-360 items-center justify-between px-8">
+    <header className="text-white1 md:bg-black1 sticky top-0 z-100 flex h-20 w-full">
+      <nav className="mx-auto flex w-full max-w-360 items-center justify-between px-2 md:px-8">
         <div className="flex gap-14.5">
           <Link to="/main" className="flex cursor-pointer items-center gap-1">
             <img src={logoImg} alt="멋쟁이사자처럼 강남대" className="w-9" />
-            <span className="text-[21px] leading-140 font-bold">
+            <span className="text-[14px] leading-140 font-bold md:text-[21px]">
               LIKELION KNU
             </span>
           </Link>
-          <div className="tracking-tight-custom flex items-center gap-12 text-[18px] leading-140 font-medium">
+          <div className="tracking-tight-custom hidden items-center gap-12 text-[18px] leading-140 font-medium md:flex">
             <NavLink
               to="/project"
               className={({ isActive }) => getActiveClass(isActive)}
@@ -58,11 +60,11 @@ function Header() {
             </NavLink>
           </div>
         </div>
-        <div className="flex items-center gap-[19.2px]">
+        <div className="items-center gap-[19.2px] md:flex">
           {isLogin ? (
             <>
               <div
-                className="flex items-center gap-2"
+                className="hidden items-center gap-2 md:flex"
                 onClick={() => navigate("/my")}
               >
                 <img
@@ -79,7 +81,7 @@ function Header() {
             <>
               <div
                 onClick={GoogleLogin}
-                className="border-white1 mr-2 flex cursor-pointer items-center rounded-lg border-[0.4px] px-5 py-2.5"
+                className="border-white1 mr-2 hidden cursor-pointer items-center rounded-lg border-[0.4px] px-5 py-2.5 md:flex"
               >
                 <img src={googleImg} alt="google" className="w-6" />
                 <span className="tracking-tight-custom ml-2.5 text-base leading-140 font-semibold">
@@ -88,13 +90,41 @@ function Header() {
               </div>
             </>
           )}
-
-          <div className="mr-1.5 hidden cursor-pointer flex-col gap-[4.5px]">
-            <ToggleBar />
-            <ToggleBar />
-            <ToggleBar />
+          <div className="flex items-center gap-3 md:hidden">
+            <img
+              src={googleImg}
+              alt="google"
+              className="w-6 rounded-[50%] bg-white p-1.5"
+            />
+            <div
+              className="mr-1.5 flex cursor-pointer flex-col gap-[4.5px]"
+              onClick={() => setIsShow((prev) => !prev)}
+            >
+              <ToggleBar />
+              <ToggleBar />
+              <ToggleBar />
+            </div>
           </div>
         </div>
+        {isShow && (
+          <div className="bg-mobile-navigation absolute top-20 left-0 flex w-full flex-col gap-4 bg-black p-5">
+            <Link to="/my" className="text-[14px] font-semibold">
+              마이페이지
+            </Link>
+            <Link to="/project" className="text-[14px] font-semibold">
+              프로젝트
+            </Link>
+            <Link to="/part/PM" className="text-[14px] font-semibold">
+              파트 소개
+            </Link>
+            <Link to="/apply" className="text-[14px] font-semibold">
+              인스타그램
+            </Link>
+            <Link to="/apply" className="text-[14px] font-semibold">
+              14기 지원하기
+            </Link>
+          </div>
+        )}
       </nav>
     </header>
   );
