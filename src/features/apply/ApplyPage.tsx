0a46df49a4b8a6dda462d5c8d5a6dbf533738ab8
pyw424 <parkyw0424@naver.com>
2026-02-13 03:24:36 +0900

    #60 feat: 에러 메세지 출력 수정

--- src/features/apply/ApplyPage.tsx
+++ src/features/apply/ApplyPage.tsx
@@ -102,8 +102,12 @@ export default function ApplyPage() {
       }
     } catch (error) {
       let msg = "서버와 연결할 수 없습니다.";
-      if (axios.isAxiosError(error) && error.response?.data?.message) {
-        msg = error.response.data.message;
+      if (axios.isAxiosError(error)) {
+        if (error.response?.data?.error?.message) {
+          msg = error.response.data.error.message;
+        } else if (error.response?.data?.message) {
+          msg = error.response.data.message;
+        }
       } else if (error instanceof Error) {
         msg = error.message;
       }
